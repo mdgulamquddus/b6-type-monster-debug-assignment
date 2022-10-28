c6ec5843217958b9937b40e9aa506774a07a2531
md-gulam-quddus <sohag.1984@gmail.com>
2022-10-28 16:27:03 +0200

    seconds floating number fixed

--- script.js
+++ script.js
@@ -140,6 +140,6 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
   document.getElementById("show-time").innerHTML = `${
-    startTime ? timeSpent : 0
+    startTime ? timeSpent.toFixed() : 0
   } seconds`;
 }, 1000);
