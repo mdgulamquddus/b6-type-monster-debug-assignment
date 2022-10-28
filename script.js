4e870675bf183b6f38df948b2e931f721cc9b7e5
md-gulam-quddus <sohag.1984@gmail.com>
2022-10-28 16:50:34 +0200

    error count is now working

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="red">${
       newLetter === " " ? "▪" : newLetter
     }</span>`;
+    errorCount += 1; //error count
   }
 
   // check if given question text is equal to user typed text
